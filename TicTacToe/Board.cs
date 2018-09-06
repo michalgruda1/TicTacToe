365494dcfec5e6667d274161f2f7d26c1778a286
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2018-09-06 15:50:48 +0200

    Add project files.

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Board
+    {
+        // member
+        private string[,] fields;
+
+        // constructor
+        public Board()
+        {
+            this.fields = new string[3, 3]
+            {
+                { "1","2","3"},
+                { "4","5","6"},
+                { "7","8","9"}
+            };
+        }
+
+        public void RenderBoard()
+        {
+            Console.WriteLine("\nLet's play a tic-tac-toe. \nType in a number 1-9 to place your mark.\n");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("|         |         |         |");
+                Console.Write("|    ");
+                if (this.fields[i, 0] == "X" || this.fields[i, 0] == "O")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Console.Write(this.fields[i, 0]);
+                Console.ResetColor();
+                Console.Write("    |    ");
+                if (this.fields[i, 1] == "X" || this.fields[i, 1] == "O")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Console.Write(this.fields[i, 1]);
+                Console.ResetColor();
+                Console.Write("    |    ");
+                if (this.fields[i, 2] == "X" || this.fields[i, 2] == "O")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Console.Write(this.fields[i, 2]);
+                Console.ResetColor();
+                Console.Write("    |\n");
+                Console.WriteLine("|         |         |         |");
+                if (i == 2) Console.WriteLine("-------------------------------");
+            }
+        }
+
+        // argument "mark" is either O or X
+        public void SetField(int row, int column, string mark)
+        {
+            this.fields[row, column] = mark;
+        }
+
+        public string GetField(int row, int column)
+        {
+            // here we simply extract field value for given number of field
+            return this.fields[row, column];
+        }
+
+        public bool IsFieldChangeable(int row, int column)
+        {
+            if (this.fields[row, column].Equals("X") || this.fields[row, column].Equals("O"))
+            {
+                Console.WriteLine("Field already has mark, choose different number", row, column);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool IsInputCorrect(int input)
+        {
+            if (input > 0 && input < 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool CheckWin(int playerNumber, string mark)
+        {
+            bool isWinner = false;
+
+            // check rows
+            for (int row = 0; row < 3; row++)
+            {
+                if (this.fields[row, 0] == mark && this.fields[row, 1] == mark && this.fields[row, 2] == mark)
+                {
+                    isWinner = true;
+                }
+            }
+
+            // check columns
+            for (int col = 0; col < 3; col++)
+            {
+                if (this.fields[0, col] == mark && this.fields[1, col] == mark && this.fields[2, col] == mark)
+                {
+                    isWinner = true;
+                }
+            }
+
+            //check diagonals
+            if (this.fields[0, 0] == mark && this.fields[1, 1] == mark && this.fields[2, 2] == mark)
+            {
+                isWinner = true;
+            }
+            else if (this.fields[0, 2] == mark && this.fields[1, 1] == mark && this.fields[2, 0] == mark)
+            {
+                isWinner = true;
+            }
+
+            /*
+            // now return
+            if (isWinner && playerNumber == 1)
+            {
+                ++winsPlayer1;
+            } else if (isWinner && playerNumber == 2)
+            {
+                ++winsPlayer2;
+            }
+            */
+            return isWinner;
+        }
+
+        public bool CheckTruce()
+        {
+            foreach (string field in this.fields)
+            {
+                // if there is any number left in the array (not all are Os and Xs), it's not truce
+                if (Int32.TryParse(field, out int number)) return false;
+            };
+            return true;
+        }
+    }
+
+    public sealed class Score
+    {
+        static readonly Score instance = new Score();
+
+        public static Score Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        public static int Games { get; set; }
+        public static int Draws { get; set; }
+        public static int Player1Wins { get; set; }
+        public static int Player2Wins { get; set; }
+
+        public Score()
+        {
+            Games = 0;
+            Draws = 0;
+            Player1Wins = 0;
+            Player2Wins = 0;
+        }
+
+        public void ConcludeGame(int gameResult)
+        {
+            Games++;
+            switch (gameResult)
+            {
+                case 0:
+                    Draws++;
+                    break;
+                case 1:
+                    Player1Wins++;
+                    break;
+                case 2:
+                    Player2Wins++;
+                    break;
+                default:
+                    throw new Exception(String.Format("Unrecognized game result - {0}", gameResult));
+            }
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("\n\n---SCORE-----");
+            Console.WriteLine("Games:    {0}", Games);
+            Console.WriteLine("Player 1: {0}", Player1Wins);
+            Console.WriteLine("Player 2: {0}", Player2Wins);
+            Console.WriteLine("Draws:    {0}\n", Draws);
+        }
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // we restart the game, after winning or truce, by breaking from the loop
+            while (true)
+            {
+
+                // initialize board
+                Board myBoard = new Board();
+
+                // which pass of the program is in, starting with 1
+                int pass = 1;
+
+                // is it player 1 or 2
+                int playerNumber = 1;
+
+                // player 1 plays with O, player 2 with X
+                string mark = "O";
+
+                // tell why we are here and what to do
+                Console.Clear();
+                Console.WriteLine("Let's play a tic-tac-toe. \nType in a number 1-9 to place your mark.\n");
+
+                // initialize info whether user tried to fill in field which already has mark in it
+                bool isTryingToOverwrite = false;
+
+                // initialize info is user input was incorrect - NaN, number out of bounds
+                bool userInputIncorrect = false;
+
+                // play, until somebody wins or truce
+                while (!(myBoard.CheckWin(playerNumber, mark) || myBoard.CheckTruce()))
+                {
+                    // odd pass is Player 1 and even is Player 2
+                    if (pass % 2 == 1)
+                    {
+                        playerNumber = 1;
+                        mark = "O";
+                    }
+                    else
+                    {
+                        playerNumber = 2;
+                        mark = "X";
+                    }
+
+                    // here we start - show board and instructions
+                    Console.Clear();
+                    myBoard.RenderBoard();
+                    Console.WriteLine("\nPlayer {0}, choose where to put your {1}", playerNumber, mark);
+
+                    // initialize scoreboard
+                    Score scoreBoard = Score.Instance;
+
+                    // user tried to ovewrite already set field in previous pass?
+                    if (isTryingToOverwrite)
+                    {
+                        Console.WriteLine("\nThis field already has mark in it. Try with different.");
+                        // reset this to false, now when you nagged the user
+                        isTryingToOverwrite = false;
+                    }
+
+                    // user provided invalid input in previous pass?
+                    if (userInputIncorrect)
+                    {
+                        Console.WriteLine("\nInput NOT correct. Should be a number between 1 and 9.");
+                        // reset this to false, now when you nagged the user
+                        userInputIncorrect = false;
+                    }
+
+                    // take input
+                    string rawInput = Console.ReadLine();
+
+                    // validate
+                    if (Int32.TryParse(rawInput, out int input) && myBoard.IsInputCorrect(input))
+                    {
+                        switch (input)
+                        {
+                            case 1:
+                                if (myBoard.IsFieldChangeable(0, 0))
+                                {
+                                    myBoard.SetField(0, 0, mark);
+                                }
+                                else
+                                {
+                                    isTryingToOverwrite = true;
+                                    continue;
+                                }
+                                break;
+                            case 2:
+                                if (myBoard.IsFieldChangeable(0, 1))
+                                {
+                                    myBoard.SetField(0, 1, mark);
+                                }
+                                else
+                                {
+                                    isTryingToOverwrite = true;
+                                    continue;
+                                }
+                                break;
+                            case 3:
+                                if (myBoard.IsFieldChangeable(0, 2))
+                                {
+                                    myBoard.SetField(0, 2, mark);
+                                }
+                                else
+                                {
+                                    isTryingToOverwrite = true;
+                                    continue;
+                                }
+                                break;
+                            case 4:
+                                if (myBoard.IsFieldChangeable(1, 0))
+                                {
+                                    myBoard.SetField(1, 0, mark);
+                                }
+                                else
+                                {
+                                    isTryingToOverwrite = true;
+                                    continue;
+                                }
+                                break;
+                            case 5:
+                                if (myBoard.IsFieldChangeable(1, 1))
+                                {
+                                    myBoard.SetField(1, 1, mark);
+                                }
+                                else
+                                {
+                                    isTryingToOverwrite = true;
+                                    continue;
+                                }
+                                break;
+                            case 6:
+                                if (myBoard.IsFieldChangeable(1, 2))
+                                {
+                                    myBoard.SetField(1, 2, mark);
+                                }
+                                else
+                                {
+                                    isTryingToOverwrite = true;
+                                    continue;
+                                }
+                                break;
+                            case 7:
+                                if (myBoard.IsFieldChangeable(2, 0))
+                                {
+                                    myBoard.SetField(2, 0, mark);
+                                }
+                                else
+                                {
+                                    isTryingToOverwrite = true;
+                                    continue;
+                                }
+                                break;
+                            case 8:
+                                if (myBoard.IsFieldChangeable(2, 1))
+                                {
+                                    myBoard.SetField(2, 1, mark);
+                                }
+                                else
+                                {
+                                    isTryingToOverwrite = true;
+                                    continue;
+                                }
+                                break;
+                            case 9:
+                                if (myBoard.IsFieldChangeable(2, 2))
+                                {
+                                    myBoard.SetField(2, 2, mark);
+                                }
+                                else
+                                {
+                                    isTryingToOverwrite = true;
+                                    continue;
+                                }
+                                break;
+                            default:
+                                userInputIncorrect = true;
+                                continue;
+                        }
+
+                        // do we have a winner?
+                        if (myBoard.CheckWin(playerNumber, mark))
+                        {
+                            Console.Clear();
+                            myBoard.RenderBoard();
+                            Console.WriteLine("****************************************** \nPlayer {0}, using {1} has won!!!", playerNumber, mark);
+                            // count win for player
+                            if (playerNumber == 1)
+                            {
+                                scoreBoard.ConcludeGame(1);
+                            }
+                            else
+                            {
+                                scoreBoard.ConcludeGame(2);
+                            }
+
+                            // display score
+                            scoreBoard.Draw();
+
+                            // exit the do loop
+                            break;
+                        }
+                        else if (myBoard.CheckTruce())
+                        // do we have a truce?
+                        {
+                            Console.Clear();
+                            myBoard.RenderBoard();
+                            Console.WriteLine("__________________________________________ \nWe have a truce, no winner here!!!");
+                            // record the draw in score, and exit the do loop
+                            scoreBoard.ConcludeGame(0);
+                            // drawscoreboard
+                            scoreBoard.Draw();
+                            break;
+                        }
+                        else
+                        {
+                            // nothing to do, so redraw the board with input from user
+                            Console.Clear();
+                            myBoard.RenderBoard();
+                            Console.WriteLine("\nPlayer {0}, choose where to put your {1}", playerNumber, mark);
+                            // drawscoreboard
+                            scoreBoard.Draw();
+
+                        }
+                    }
+                    else
+                    {
+                        userInputIncorrect = true;
+                        continue;
+                    }
+                    pass++;
+                }
+
+                // rerun entire thingy, if main do loop ends by winning or draw
+                Console.WriteLine("Press a key to reset the game");
+                Console.ReadKey();
+                Main(null);
+                Console.Read();
+            }
+
+        }
+    }
+}
